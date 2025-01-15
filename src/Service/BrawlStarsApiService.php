d809159f77565f65cb7d58c922d78d7a3391d926
Hrivaux <hrivaux@sfr.fr>
2025-01-15 11:47:33 +0100

    amelioration Service

--- src/Service/BrawlStarsApiService.php
+++ src/Service/BrawlStarsApiService.php
@@ -2,35 +2,11 @@
 
 namespace App\Service;
 
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-
-class BrawlStarsApiService
+class BrawlStarsApiService extends AbstractApiService
 {
-    private HttpClientInterface $client;
-    private string $apiToken;
-
-    public function __construct(HttpClientInterface $client)
-    {
-        $this->client = $client;
-        $this->apiToken = $_ENV['BRAWLSTARS_API_TOKEN'] ?? '';
-    }
-
-    
     public function fetchBrawlers(): array
     {
         $apiUrl = 'https://api.brawlstars.com/v1/brawlers';
-
-        try {
-            $response = $this->client->request('GET', $apiUrl, [
-                'headers' => [
-                    'Authorization' => 'Bearer ' . $this->apiToken,
-                ],
-            ]);
-
-            return $response->toArray(); // Convertit la réponse JSON en tableau PHP
-        } catch (\Exception $e) {
-            // Gestion des erreurs
-            throw new \Exception('Erreur lors de la récupération des brawlers : ' . $e->getMessage());
-        }
+        return $this->request($apiUrl);
     }
 }
